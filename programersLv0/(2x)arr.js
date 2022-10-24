baa52f1c2909950c509a729b0cb7cefe1796281a
par jin sung <parkgutime@gmail.com>
2022-10-24 12:31:16 +0900

    commit

---
+++ programersLv0/(2x)arr.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    let arr = [...numbers];
+    
+    for (let i = 0; i < arr.length; i++){
+      arr[i] = arr[i] * 2;
+    }
+        return arr
+    }
--- programersLv0/arrAverage.js
+++ programersLv0/arrAverage.js
@@ -10,4 +10,5 @@ function solution(numbers) {
 //평균값을 구하는 문제 numbers.length;
 
 //1. 배열을 나타내고, numbers = []
-//2. 배열의 숫자의 총합을 합하고, numbers.length 만큼 나눈다?
+//2. 배열의 숫자의 총합을 reduce함수를 사용해서 합하고, numbers.length 만큼 나눈다
+//3. 함수 내 에서 return 값을 구하고 지역함수에서 나와서 다시 나눠준다.
